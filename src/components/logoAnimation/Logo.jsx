d0a5f6a6a890d0e6ef1f0173bae6ab8fdc6d0174
walter-202 <wfernando.aguilarm@gmail.com>
2023-01-24 01:07:14 -0400

    gsap removed fr archives

--- src/components/logoAnimation/Logo.jsx
+++ src/components/logoAnimation/Logo.jsx
@@ -3,7 +3,7 @@ import './logo.scss'
 // import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
 // import gsap from 'gsap'
 
-// const Logo = () => {
+const Logo = () => {
 //   const bgRef = React.useRef()
 //   const outLineRef = React.useRef()
 //   const outLineRef2 = React.useRef()
@@ -40,10 +40,12 @@ import './logo.scss'
 //     }, 4000)
 //   }, [])
 
-  return (
-    <div className="logo-container switch__stroke-color" ref={bgRef}>
+  // return (
+    <div className="logo-container switch__stroke-color" 
+    // ref={bgRef}
+    >
       <svg
-        ref={solidLogoRef}
+        // ref={solidLogoRef}
         xmlns="http://www.w3.org/2000/svg"
         className="home-logo solid-logo"
         viewBox="-127.1 -160.1 254.2 270.2"
@@ -59,17 +61,17 @@ import './logo.scss'
         viewBox="-127.1 -160.1 254.2 270.2"
       >
         <path
-          ref={outLineRef}
+          // ref={outLineRef}
           className="solid-logo"
           d="M -50 110 L 0 0 L 50 110 L 127 -67 H 93 L 50 30 L 0 -75 L -50 30 L -92 -67 H -127 L -50 110  " />
         <path
           className="solid-logo"
           d="M -51 -13 L 0 -100 L 50 -10 L 60 -50 L 0 -160 L -60 -50 L -51 -13"
-          ref={outLineRef2}
+          // ref={outLineRef2}
         />
       </svg>
     </div>
-  )
-// }
+  // )
+}
 
 export default Logo
