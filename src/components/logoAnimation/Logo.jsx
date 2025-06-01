4813d03a37ebc681f84ddd6cc3b0c2d3f7fd28c6
walter-202 <79384958+walter-202@users.noreply.github.com>
2025-06-01 02:20:13 -0400

    Updated package-lock.json

--- src/components/logoAnimation/Logo.jsx
+++ src/components/logoAnimation/Logo.jsx
@@ -1,59 +1,68 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
+import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
 import './logo.scss'
-// import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
-// import gsap from 'gsap'
 
 const Logo = () => {
-//   const bgRef = React.useRef()
-//   const outLineRef = React.useRef()
-//   const outLineRef2 = React.useRef()
-//   const solidLogoRef = React.useRef()
+  const bgRef = useRef(null)
+  const outLineRef = useRef(null)
+  const outLineRef2 = useRef(null)
+  const solidLogoRef = useRef(null)
 
-//   React.useEffect(() => {
-//     gsap.registerPlugin(DrawSVGPlugin)
-//     setTimeout(() => {
-//       gsap
-//         .timeline()
-//         .to(bgRef.current, {
-//           duration: 1,
-//           opacity: 1,
-//         })
-//         .from(outLineRef.current, {
-//           drawSVG: 0,
-//           duration: 4,
-//         })
-//         .from(outLineRef2.current, {
-//           drawSVG: 0,
-//           duration: 4,
-//         })
-//       gsap.fromTo(
-//         solidLogoRef.current,
-//         {
-//           opacity: 0,
-//         },
-//         {
-//           opacity: 0.5,
-//           delay: 4,
-//           duration: 8,
-//         },
-//       )
-//     }, 4000)
-//   }, [])
+  useEffect(() => {
+    gsap.registerPlugin(DrawSVGPlugin)
+    
+    const timeline = gsap.timeline()
+    
+    setTimeout(() => {
+      timeline
+        .to(bgRef.current, {
+          duration: 1,
+          opacity: 1,
+        })
+        .from(outLineRef.current, {
+          drawSVG: 0,
+          duration: 4,
+        })
+        .from(outLineRef2.current, {
+          drawSVG: 0,
+          duration: 4,
+        })
 
-  // return (
-    <div className="logo-container switch__stroke-color" 
-    // ref={bgRef}
-    >
+      gsap.fromTo(
+        solidLogoRef.current,
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 0.5,
+          delay: 4,
+          duration: 8,
+        }
+      )
+    }, 4000)
+
+    return () => {
+      timeline.kill()
+    }
+  }, [])
+
+  return (
+    <div className="logo-container switch__stroke-color" ref={bgRef}>
       <svg
-        // ref={solidLogoRef}
+        ref={solidLogoRef}
         xmlns="http://www.w3.org/2000/svg"
         className="home-logo solid-logo"
         viewBox="-127.1 -160.1 254.2 270.2"
       >
-        
-              <path className="fill__color home-logo" d="M -50 110 L 0 0 L 50 110 L 127 -67 H 93 L 50 30 L 0 -75 L -50 30 L -92 -67 H -127 L -50 110 " />
-              <path  d="M -51 -13 L 0 -100 L 50 -10 L 60 -50 L 0 -160 L -60 -50 L -51 -13" fill="#797878"/>
-            
+        <path 
+          className="fill__color home-logo" 
+          d="M -50 110 L 0 0 L 50 110 L 127 -67 H 93 L 50 30 L 0 -75 L -50 30 L -92 -67 H -127 L -50 110 " 
+        />
+        <path  
+          d="M -51 -13 L 0 -100 L 50 -10 L 60 -50 L 0 -160 L -60 -50 L -51 -13" 
+          fill="#797878"
+        />
       </svg>
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -61,17 +70,18 @@ const Logo = () => {
         viewBox="-127.1 -160.1 254.2 270.2"
       >
         <path
-          // ref={outLineRef}
+          ref={outLineRef}
           className="solid-logo"
-          d="M -50 110 L 0 0 L 50 110 L 127 -67 H 93 L 50 30 L 0 -75 L -50 30 L -92 -67 H -127 L -50 110  " />
+          d="M -50 110 L 0 0 L 50 110 L 127 -67 H 93 L 50 30 L 0 -75 L -50 30 L -92 -67 H -127 L -50 110" 
+        />
         <path
           className="solid-logo"
           d="M -51 -13 L 0 -100 L 50 -10 L 60 -50 L 0 -160 L -60 -50 L -51 -13"
-          // ref={outLineRef2}
+          ref={outLineRef2}
         />
       </svg>
     </div>
-  // )
+  )
 }
 
-export default Logo
+export default React.memo(Logo)
